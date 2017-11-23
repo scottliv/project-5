c046ad3e90c902c5925fb35428cb3f321c242d08
Scott <scottdlivingstone@gmail.com>
2017-11-23 11:03:24 -0800

    add categories and tags to archive page

--- page-archives.php
+++ page-archives.php
@@ -14,9 +14,28 @@ get_header(); ?>
 
 			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'archive' ); ?>
+				<?php
+					echo sprintf('<a href="%1s">%2s</a>', esc_url(get_permalink()), get_the_title());
+				?>
 
 			<?php endwhile; // End of the loop. ?>
+
+			<?php wp_list_categories(); ?>
+
+<?php
+	$tags = get_tags();
+?>
+
+<div class="post_tags">
+<?php
+foreach ( $tags as $tag ):
+		$tag_link = get_tag_link( $tag->term_id );
+?>
+	<a href="<?php echo $tag_link ?>" title="<?php echo $tag->name ?> Tag"> <?php echo $tag->name ?> </a>
+
+<?php endforeach; ?>
+</div>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- template-parts/content-archive.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo sprintf('<a href="%1s">%2s</a>', esc_url(get_permalink()), get_the_title());
-?>
